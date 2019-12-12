6f1718b359cf725791502be84ba817fdbb754fee
mehisringit <mehis.muldma@ringit.ee>
2019-12-12 13:41:15 +0200

    Implemented user repository and GET endpoint for retrieving user list

--- src/main/java/ee/ringit/userManagement/controllers/UserController.java
+++ src/main/java/ee/ringit/userManagement/controllers/UserController.java
@@ -1,8 +1,23 @@
 package ee.ringit.userManagement.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+
+    // GET, POST save(), DELETE deleteAll(), delete(userId), PUT/PATCH for updating
+
+    // Get all users findAll()
+    // Get single user userId findById(userId)
+
+    @GetMapping
+    public String getUsers() {
+        System.out.println("anna kasutajad");
+        return "users/list";
+    }
+
 }
 
---
+++ src/main/java/ee/ringit/userManagement/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package ee.ringit.userManagement.repositories;
+
+import ee.ringit.userManagement.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
